4a5865e73b7a7fc60dea5053af23ad2afaec908a
einfanger <inf21001@byui.edu>
2025-09-19 17:58:11 -0600

    I fixed up the code and it's working!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What grade do you have in the class?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You have a: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You killed it!");
+        }
+        else
+        {
+            Console.WriteLine("Hit the books and you'll see results! Studying.. I mean. Don't actually hit the books, they're probably expensive.");
+        }
     }
 }
