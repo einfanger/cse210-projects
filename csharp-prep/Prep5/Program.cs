217911365aff1aa75398620f82a1a73eb67831fc
einfanger <inf21001@byui.edu>
2025-09-24 17:27:27 -0600

    Birth Year Done!

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("Please enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squaredNumber, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        int age = DateTime.Now.Year - birthYear;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        PromptUserBirthYear(out int birthYear);
+        int squared = SquareNumber(number);
+        DisplayResult(name, squared, birthYear);
     }
 }
